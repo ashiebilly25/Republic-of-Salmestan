c9ca62793e7ae0f7e278c7ee52881b0b7d43d293
NyanNya <ashiebilly25@gmail.com>
2026-09-22 21:17:48 -0400

    Update d3-linegraph.js

--- out/html/d3-linegraph.js
+++ out/html/d3-linegraph.js
@@ -59,9 +59,9 @@ d3.linegraph = function(noTicks, noDots, parties, partyColors, partyNames, dataM
 
       // Declare the y (vertical position) scale.
       if (!dataMax) {
-          const maxSPD = d3.max(data, d => d.spd);
-          const maxNSDAP = d3.max(data, d => d.nsdap);
-          dataMax = maxSPD >= maxNSDAP ? maxSPD + 10 : maxNSDAP + 10;
+          const maxSDLP = d3.max(data, d => d.sdlp);
+          const maxAMC = d3.max(data, d => d.amc);
+          dataMax = maxSDLP >= maxAMC ? maxSDLP + 10 : maxAMC + 10;
           dataMin = 0;
       }
       const yScale = d3.scaleLinear([dataMin, dataMax], [height - marginBottom, marginTop]);
